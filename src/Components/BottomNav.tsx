c2f760e07b162e6425dd97ef400e1f9b5793f311
Yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-10-21 19:32:41 +0800

    Edited: Adjusted scroll speed

--- src/Components/BottomNav.tsx
+++ src/Components/BottomNav.tsx
@@ -4,14 +4,14 @@ import AnimatedButtonHover from "./AnimatedButtonHover";
 const BottomNav = () => {
   return (
     <div className="text-white my-48 flex gap-10 justify-between pr-44">
-      <Link to="work" smooth={true} duration={100}>
+      <Link to="work" smooth={true} duration={500}>
         <AnimatedButtonHover Title="WORK" />
       </Link>
       <div className="flex items-center gap-20 md:gap-40">
-        <Link to="workskillsect" smooth={true} duration={100}>
+        <Link to="workskillsect" smooth={true} duration={500}>
           <AnimatedButtonHover Title="ABOUT" />
         </Link>
-        <Link to="workskillsect" smooth={true} duration={100}>
+        <Link to="workskillsect" smooth={true} duration={500}>
           <AnimatedButtonHover Title="CONTACT" />
         </Link>
       </div>
